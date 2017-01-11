27745af70f8eef978d19ff8360fc40a4c3f4ca03
Gabriel Guedes Pereira Damaso <gabriel.damaso@sigma-systems.com>
2017-01-11 15:27:05 +0000

    http service (wip)

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule }               from '@angular/core';
 import { BrowserModule }          from '@angular/platform-browser';
 import { FormsModule }            from '@angular/forms';
+import { HttpModule }             from '@angular/http';
+
+// Imports for  loading & configuring in-memory web api
+import { InMemoryWebApiModule }   from 'angular-in-memory-web-api';
+import { InMemoryDataService }    from './in-memory-data.service';
 
 import { AppComponent }           from './app.component';
 import { HeroDetailComponent }    from './hero-detail.component';
@@ -14,7 +19,9 @@ import {AppRoutingModule}         from './app-routing.module';
   imports: [
       BrowserModule,
       FormsModule,
-      AppRoutingModule
+      AppRoutingModule,
+      HttpModule,
+      InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   declarations: [
     AppComponent,
